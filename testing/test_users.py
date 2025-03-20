12002a9ae4aa85978bb30cd4163763f955d2b532
gteodora <galateica@hotmail.com>
2025-03-20 22:21:15 +0100

    Initial commit

---
+++ testing/test_users.py
@@ -0,0 +1,11 @@
+import requests
+
+BASE_URL =  "https://jsonplaceholder.typicode.com"
+
+def test_get_users():
+    response = requests.get(f"{BASE_URL}/users")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+
+    
